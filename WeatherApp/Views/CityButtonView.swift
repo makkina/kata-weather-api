591ada74abbb3795ac45cd1289649ff97d9d7a97
Makkina <efe@appamist.com>
2020-08-20 12:15:06 +0200

    Create custom UIButton for cities

---
+++ WeatherApp/Views/CityButtonView.swift
@@ -0,0 +1,19 @@
+//
+//  CityButtonView.swift
+//  WeatherApp
+//
+//  Created by Makkina on 20/08/2020.
+//  Copyright © 2020 Makkina. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CityButtonView: UIButton {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.layer.cornerRadius = 10
+    }
+}
