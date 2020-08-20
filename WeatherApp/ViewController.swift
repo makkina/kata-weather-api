21e97c067c77c27d899c1efa5ee7c07ae802e03f
Makkina <efe@appamist.com>
2020-08-20 11:10:43 +0200

    Create Xcode project

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Makkina on 20/08/2020.
+//  Copyright © 2020 Makkina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
